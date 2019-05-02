764a84fe5ff9c979f121a5aa497d2a93c5f82ab3
Nathan Le Granvallet <nathan@legranvallet.fr>
2019-05-02 16:08:36 +0200

    Commit @ jeudi 2 mai 2019, 16:08:36 (UTC+0200)

---
+++ cmake-build-debug/CMakeFiles/NexetupC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NexetupC.dir/src/main.c.o"
+  "NexetupC.pdb"
+  "NexetupC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NexetupC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,20 @@
+/**
+ * @file main.c
+ *
+ * @brief Ceci est le main, vive le main
+ * @date 04/03/2019
+ * @author Nathan Le Granvallet
+ *
+ * @copyright Nexelis
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+int main (void)
+{
+
+    return 0;
+
+}
